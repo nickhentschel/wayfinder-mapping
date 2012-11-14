6c85670e57a8dd77d4655fc4bbf828d6272926e7
Nick Hentschel <hentschel.nicholas@gmail.com>
2012-11-14 18:02:45 -0500

    updating splash and style.css

--- application/views/splash.php
+++ application/views/splash.php
@@ -1,3 +1,6 @@
 <div class="center-content">
 	<h3>Welcome to the Wayfinder Map Builder.</h3>
+	<div class="options">
+		<a href="uploadmap">Upload a new Map</a>|<a href="#">Choose an Existing Map</a>
+	</div>
 </div>
